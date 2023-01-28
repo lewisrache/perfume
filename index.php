abac946368a370419047fc74835bfc97f497f7f1
Rachel Lewis <rachel@tuchanka.local>
2023-01-27 20:39:25 -0500

    landing page with buttons

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="utf-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<link rel="stylesheet" href="bootstrap-sortable-master/Contents/bootstrap-sortable.css">
+<link rel="stylesheet" href="bootstrap-chosen-master/bootstrap-chosen.css">
+<script src="bootstrap-sortable-master/Scripts/bootstrap-sortable.js"></script>
+<script src="bootstrap-sortable-master/Scripts/moment.min.js"></script>
+<script src="http://harvesthq.github.io/chosen/chosen.jquery.js"></script>
+</head>
+<body>
+<div class="col-lg-3">
+	<input type="button" onclick="window.location='collection_manager.php'" value="Manage Collections"/>
+	<input type="button" onclick="window.location='input-cards.php'" value="Create New Perfumes"/>
+	<input type="button" onclick="window.location='add_cards.php'" value="Assign Categories and Number"/>
+	<input type="button" onclick="window.location='cards_ui.php'" value="View Perfumes"/>
+</div>
+</body>
+</html>
